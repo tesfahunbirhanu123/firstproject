efc37a37dceec71c13ae2712bbf809445b44eafa
Birhanu <tesfahunbirhanu123@gmail.com>
2025-08-17 12:55:03 +0200

    My first calculator

--- hello.py
+++ hello.py
@@ -1,1 +1,5 @@
-print("Hello python!")
+print("My fisrt calculator")
+x  = int(input("please enter the first number:"))
+y = int(input("Please inter the second number:"))
+sum = x + y
+print(f"the sum of two numbers is: {sum}")
