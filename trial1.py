58fccdbc7a8b7e777e016892c63180cb5cc66d5d
Birhanu Tesfahun <75525910+tesfahunbirhanu123@users.noreply.github.com>
2025-08-17 13:12:35 +0200

    Create trial1.py
    new file updates

---
+++ trial1.py
@@ -0,0 +1,5 @@
+print("My fisrt calculator")
+x  = int(input("please enter the first number:"))
+y = int(input("Please inter the second number:"))
+sum = x + y
+print(f"the sum of two numbers is: {sum}")
